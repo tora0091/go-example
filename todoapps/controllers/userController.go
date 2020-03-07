0431a0e78bb10c7765e72e7df80f6c6d2714a547
tora0091 <tora0091@yahoo.co.jp>
2020-03-07 18:19:25 +0900

    modify

--- todoapps/controllers/userController.go
+++ todoapps/controllers/userController.go
@@ -1,12 +1,13 @@
 package controllers
 
 import (
-	"go-example/todoapps/database"
-	"go-example/todoapps/jsons"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	"go-example/todoapps/database"
+	"go-example/todoapps/jsons"
 )
 
 // curl -v -X GET http://localhost:8080/api/v1/users
